a455f53513c8c2a1b7d1b6735b439a480f6033e6
coolsoham <88109063+coolsoham@users.noreply.github.com>
2026-04-12 00:34:16 +0530

    modified:   Backend/config/db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,11 +1,25 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // required for Supabase
-  },
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // Production (Supabase)
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  // Local DB
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 
 pool.connect()
   .then(() => console.log('✅ PostgreSQL Connected'))
