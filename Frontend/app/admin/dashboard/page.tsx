98b4127ee0719a94717c8f080f86aa998d01e346
coolsoham <88109063+coolsoham@users.noreply.github.com>
2026-04-11 23:17:38 +0530

    fix prerender issue for admin dashboard

--- Frontend/app/admin/dashboard/page.tsx
+++ Frontend/app/admin/dashboard/page.tsx
@@ -3,7 +3,7 @@
 
 import { useState,useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
-
+export const dynamic = "force-dynamic";
 export default function AdminDashboard(){
 const searchParams = useSearchParams()
 const [homeSection, setHomeSection] = useState('')
